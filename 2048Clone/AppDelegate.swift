e52c83540ee1f1c99018190132fae53d4c678dce
HUONG Nguyen Thi 1 (VTI.D1) <huong.nguyenthi1@vti.com.vn>
2021-06-10 09:17:28 +0700

    initial project

---
+++ 2048Clone/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  2048Clone
+//
+//  Created by Huong Nguyen on 10/06/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ 2048Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2048Clone
+//
+//  Created by Huong Nguyen on 10/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
